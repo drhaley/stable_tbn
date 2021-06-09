8765a14a6c3e80a63b8b13e059e96920e61932c1
drhaley <14882746+drhaley@users.noreply.github.com>
2021-06-08 17:45:37 -0700

    efficiency improvements to Hilbert Basis calculations

--- source/formulations/hilbert_basis.py
+++ source/formulations/hilbert_basis.py
@@ -1,13 +1,13 @@
-import os, sys
+import os
+import sys
 from random import randint
 import subprocess
 import numpy as np
-from typing import List, Tuple, Dict, Any, Set
+from typing import List, Dict, Any
 from math import inf as infinity
 
 from source.formulations.abstract import Formulation as AbstractFormulation
 from source.tbn import Tbn
-from source.solver_adapters.abstract import Model
 from source.configuration import Configuration
 from source.polymer import Polymer
 
@@ -20,72 +20,164 @@ class Formulation(AbstractFormulation):
         """
         populates self.model with the variables and constraints needed to solve a formulation
         """
+        # if constrain_quantities is enabled, truncates the Hilbert basis based upon available monomer counts
+        #  sadly it does not seem to improve the runtime to enable this
+        constrain_quantities = False
+
         self._run_asserts()
         monomers_and_slack_as_matrix = self._project_tbn_to_column_matrix(self.tbn)
-        polymer_basis = self._get_hilbert_basis_from_matrix(monomers_and_slack_as_matrix)
+        polymer_basis = self._get_hilbert_basis_from_matrix(monomers_and_slack_as_matrix,
+                tbn = self.tbn if constrain_quantities else None)
         self._display_polymer_basis(polymer_basis)
-        self._populate_model_from_polymer_basis(polymer_basis)
+        self._populate_model_from_tbn_and_polymer_basis(self.tbn, polymer_basis)
 
     @staticmethod
     def _project_tbn_to_column_matrix(tbn: Tbn) -> np.array:
         monomer_types = list(tbn.monomer_types())
         limiting_domain_types = list(tbn.limiting_domain_types())
-        filtered_limiting_domain_types = list(tbn.limiting_domain_types(filter_ties=True))
         monomer_matrix = np.array([
-            [monomer.net_count(domain) for domain in limiting_domain_types]
+            [-monomer.net_count(domain) for domain in limiting_domain_types]
                 for monomer in monomer_types
         ], np.int64).T
-        if filtered_limiting_domain_types:  # is not empty
-            slack_matrix = np.array([
-                [1 if filtered_domain == domain else 0 for filtered_domain in filtered_limiting_domain_types]
-                    for domain in limiting_domain_types
-            ], np.int64)
-            full_matrix = np.hstack((monomer_matrix, slack_matrix))
-        else:
-            full_matrix = monomer_matrix
-        return full_matrix
+
+        return monomer_matrix
 
     @staticmethod
-    def _get_hilbert_basis_from_matrix(matrix: np.array, quiet: bool = False) -> np.array:
+    def _get_hilbert_basis_from_matrix(matrix: np.array, tbn: Tbn = None, quiet: bool = False) -> np.array:
+        # if a TBN is specified, the Hilbert basis will be constrained to the quantities of the monomers in the TBN
         temporary_filename_prefix = os.path.join(os.getcwd(), f"TEMP_4ti2_CALLOUT_{randint(0,10000)}")
         matrix_filename = temporary_filename_prefix + '.mat'
-        hilbert_basis_filename = temporary_filename_prefix + '.hil'
+        relations_filename = temporary_filename_prefix + '.rel'
+        signs_filename = temporary_filename_prefix + '.sign'
+        rhs_filename = temporary_filename_prefix + '.rhs'
+        homogenous_basis_filename = temporary_filename_prefix + '.zhom'
+        inhomogenous_basis_filename = temporary_filename_prefix + '.zinhom'
+
+        number_of_domain_types = matrix.shape[0]
+        number_of_monomer_types = matrix.shape[1]
+
+        if tbn:
+            identity_matrix = np.identity(number_of_monomer_types, np.int64)
+            matrix = np.concatenate((matrix, identity_matrix))
 
         # write a temporary matrix file
         with open(matrix_filename, 'w') as outFile:
             # shape is first line of the .mat file
-            outFile.write(f"{matrix.shape[0]} {matrix.shape[1]}\n")
+            number_of_rows = number_of_domain_types + (number_of_monomer_types if tbn else 0)
+            outFile.write(f"{number_of_rows} {number_of_monomer_types}\n")
             for row in matrix:
                 for entry in row:
                     outFile.write(f"{entry} ")
                 outFile.write('\n')
 
-        subprocess.call(["4ti2-hilbert", temporary_filename_prefix, "-q" if quiet else ""])
+        # write a temporary relations file
+        with open(relations_filename, 'w') as outFile:
+            entries = number_of_domain_types + (number_of_monomer_types if tbn else 0)
+            outFile.write(f"1 {entries}\n")
+            outFile.write(' '.join(['>']*number_of_domain_types))
+            if tbn:
+                outFile.write(' ')
+                outFile.write(' '.join(['<']*number_of_monomer_types))
+            outFile.write('\n')
 
-        # read and interpret response
-        with open(hilbert_basis_filename, 'r') as inFile:
-            # shape is first line of the file
-            shape_as_string = inFile.readline()
-            shape = tuple(int(x) for x in shape_as_string.split())
+        # write a temporary signs file
+        with open(signs_filename, 'w') as outFile:
+            outFile.write(f"1 {number_of_monomer_types}\n")
+            outFile.write(' '.join(['1']*number_of_monomer_types))
+            outFile.write('\n')
 
-            flat_array = np.array(inFile.read().split(), np.int64)
-            basis = flat_array.reshape(*shape).T
+        # write a temporary right hand sides file
+        with open(rhs_filename, 'w') as outFile:
+            entries = number_of_domain_types + (number_of_monomer_types if tbn else 0)
+            outFile.write(f"1 {entries}\n")
+            outFile.write(' '.join(['0']*number_of_domain_types))
+            if tbn:
+                outFile.write(' ')
+                outFile.write(' '.join([str(tbn.count(monomer)) for monomer in tbn.monomer_types()]))
+            outFile.write('\n')
 
-        # clean up
-        os.remove(matrix_filename)
-        os.remove(hilbert_basis_filename)
+        try:
+            subprocess.call(["4ti2-zsolve", temporary_filename_prefix] + (['-q'] if quiet else []))
 
-        return basis
+            # read and interpret response
+            with open(homogenous_basis_filename, 'r') as inFile:
+                # shape is first line of the file
+                shape_as_string = inFile.readline()
+                shape = tuple(int(x) for x in shape_as_string.split())
 
-    def _populate_model_from_polymer_basis(self, basis: np.array) -> Model:
+                flat_array = np.array(inFile.read().split(), np.int64)
+                homogenous_basis = flat_array.reshape(*shape)
+
+            with open(inhomogenous_basis_filename, 'r') as inFile:
+                # shape is first line of the file
+                shape_as_string = inFile.readline()
+                shape = tuple(int(x) for x in shape_as_string.split())
+
+                flat_array = np.array(inFile.read().split(), np.int64)
+                inhomogenous_basis = flat_array.reshape(*shape)
+
+                # remove the row of all zeros
+                inhomogenous_basis = inhomogenous_basis[~np.all(inhomogenous_basis == 0, axis=1)]
+
+            if homogenous_basis.size == 0:
+                basis = inhomogenous_basis.T
+            elif inhomogenous_basis.size == 0:
+                basis = homogenous_basis.T
+            else:
+                basis = np.concatenate(homogenous_basis, inhomogenous_basis).T
+
+            print('='*80)
+            print(homogenous_basis)
+            print('-' * 80)
+            print(inhomogenous_basis)
+            print('-' * 80)
+            print(basis)
+            print('=' * 80)
+        except FileNotFoundError:
+            print("4ti2 was not able to complete")
+            basis = None
+        finally:
+            # clean up
+            for filename in [
+                    matrix_filename,
+                    relations_filename,
+                    signs_filename,
+                    rhs_filename,
+                    inhomogenous_basis_filename,
+                    homogenous_basis_filename,
+                    ]:
+                try:
+                    os.remove(filename)
+                except FileNotFoundError:
+                    pass
+        if basis.size > 0:
+            return basis
+        else:
+            raise AssertionError("the callout to 4ti2 did not generate a Hilbert basis")
+
+    def _populate_model_from_tbn_and_polymer_basis(self, tbn: Tbn, basis: np.array) -> None:
         self.polymer_basis = basis
 
-        self.model.set_big_m(500)  # TODO: replace 500 with appropriate bound
+        # upper bound on how many total monomers can be in non-singleton polymers
+        upper_bound_on_total_monomers_in_complexes = sum(
+            tbn.count(monomer_type) * (1 + abs(monomer_type.net_count(domain_type)))
+            for monomer_type in tbn.limiting_monomer_types()
+            for domain_type in tbn.limiting_domain_types()
+        )
+        monomer_counts = [tbn.count(monomer) for monomer in tbn.monomer_types()]
+        upper_bound_on_total_monomers_in_complexes = min(
+            upper_bound_on_total_monomers_in_complexes,
+            sum(monomer_counts)   #total number of monomers
+        )
+
+        BIG_M = upper_bound_on_total_monomers_in_complexes
+
+        self.model.set_big_m(BIG_M)
 
         # declare variables
         size_of_basis_vectors, number_of_basis_vectors = basis.shape
         self.basis_coefficients = [
-            self.model.int_var(0, 500, f"basis_coefficient_{i}")  # TODO: replace 500 with an appropriate upper bound
+            self.model.int_var(0, BIG_M, f"basis_coefficient_{i}")
                 for i in range(number_of_basis_vectors)
         ]
 
--- tests/formulations/test_hilbert_basis.py
+++ tests/formulations/test_hilbert_basis.py
@@ -1,11 +1,8 @@
 import unittest
 import numpy as np
-from math import inf as infinity
 
 from source.tbn import Tbn
 from source.formulations.hilbert_basis import Formulation
-from source.monomer import Monomer
-from source.domain import Domain
 
 
 class TestGraverBasis(unittest.TestCase):
@@ -17,21 +14,29 @@ class TestGraverBasis(unittest.TestCase):
             test_tbn = Tbn.from_string("a* b* \n a b \n a* \n b*")
             actual_matrix = Formulation._project_tbn_to_column_matrix(test_tbn)
             expected_matrix = np.array([
-                [ 1, 1],  # a b
-                [-1,-1],  # a* b*
-                [-1, 0],  # a*
-                [ 0,-1],  # b*
-                [ 1, 0],  # a slack
-                [ 0, 1],  # b slack
+                [-1,-1],  # a b
+                [ 1, 1],  # a* b*
+                [ 1, 0],  # a*
+                [ 0, 1],  # b*
             ], np.int64).T
             self.assertTrue(np.array_equal(expected_matrix, actual_matrix))
 
         with self.subTest("no slack version"):
-            test_tbn = Tbn.from_string("a* b* \n a b")  # no slack this time
+            test_tbn = Tbn.from_string("a* b* \n a b")  # no slack this time, should default to stars being limiting
             actual_matrix = Formulation._project_tbn_to_column_matrix(test_tbn)
             expected_matrix = np.array([
-                [-1,-1],  # a b
-                [ 1, 1],  # a* b*
+                [ 1, 1],  # a b
+                [-1,-1],  # a* b*
+            ], np.int64).T
+            self.assertTrue(np.array_equal(expected_matrix, actual_matrix))
+
+        with self.subTest("second no slack version"):
+            test_tbn = Tbn.from_string("a \n a* b* \n a b")
+            actual_matrix = Formulation._project_tbn_to_column_matrix(test_tbn)
+            expected_matrix = np.array([
+                [ 1, 1],  # a b
+                [-1,-1],  # a* b*
+                [ 1, 0],  # a
             ], np.int64).T
             self.assertTrue(np.array_equal(expected_matrix, actual_matrix))
 
@@ -40,9 +45,11 @@ class TestGraverBasis(unittest.TestCase):
             [1, -2, 1],
         ], np.int64)
         expected_hilbert_basis = np.array([
+            [1, 0, 0],
             [2, 1, 0],
             [1, 1, 1],
             [0, 1, 2],
+            [0, 0, 1],
         ], np.int64).T
         hilbert_basis = Formulation._get_hilbert_basis_from_matrix(A, quiet=True)
         self.assertEqual(expected_hilbert_basis.shape, hilbert_basis.shape)  # dimensions should be equal
